985d1cb1413065a7af71193c3fceed63ee21c75e
Joseph Cotton <joe@lightningflash.net>
2018-09-29 20:58:34 -0500

    Testing some of the FUN extension

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@
 
 // include the SevenSegmentTM1637 library
 #include "SevenSegmentTM1637.h"
+#include "SevenSegmentFun.h"
 
 /* initialize global TM1637 Display object
 *  The constructor takes two arguments, the number of the clock pin and the digital output pin:
@@ -27,32 +28,40 @@
 */
 const byte PIN_CLK = A5; // define CLK pin (any digital pin)
 const byte PIN_DIO = A4; // define DIO pin (any digital pin)
-SevenSegmentTM1637 display(PIN_CLK, PIN_DIO);
+SevenSegmentFun display(PIN_CLK, PIN_DIO);
 
 // run setup code
 void setup()
 {
   Serial.begin(57600);        // initializes the Serial connection @ 9600 baud
   display.begin();           // initializes the display
-  display.setBacklight(100); // set the brightness to 100 %
-  display.print("INIT");     // display INIT on the display
-  delay(1000);               // wait 1000 ms
+  display.setBacklight(50); // set the brightness to 100 %
+  // display.print("INIT");     // display INIT on the display
+  // delay(1000);               // wait 1000 ms
 };
 
 // run loop (forever)
 void loop()
 {
-  display.print("LOOP");                 // display LOOP on the display
-  delay(1000);                           // wait 1000 ms
-  display.print("COUNTING SOME DIGITS"); // print COUNTING SOME DIGITS
+  display.snake(5);
   display.clear();                       // clear the display
-  for (uint8_t i = 0; i < 100; i++)
+  display.nightrider(5);
+  display.clear();                       // clear the display
+  display.bombTimer(1,30,1200);
+  display.clear();                       // clear the display
+  for (uint8_t i = 0; i <= 100; i=i+25)
+  {                   // loop from 0 to 100
+    display.printLevelVertical(i);
+    delay(200);       // wait 100 ms
+  };
+  display.clear(); // clear the display
+  for (uint16_t i = 0; i < 500; i++)
   {                   // loop from 0 to 100
     display.print(i); // display loop counter
-    delay(100);       // wait 100 ms
+    delay(25);       // wait 100 ms
   };
   display.clear();       // clear the display
-  display.print("SUCC"); // print SUCC for success
-  display.blink();       // blink SUCC
-  delay(1000);           // wait 1000 ms
+  // display.print("SUCC"); // print SUCC for success
+  // display.blink();       // blink SUCC
+  delay(500);           // wait 1000 ms
 };
